f8560ecb66e8f52efb2083e3b22e9fde0f0a6c6b
tanishkpandey <pandeytanishk@gmail.com>
2025-04-09 10:38:32 +0530

    fix: reduced padding to make the pages more symmetric.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ import { Experience } from "@/components/Experience";
 export default function Home() {
   return (
     <div className="bg-[#f7f7f752] min-h-screen">
-      <div className="container max-w-screen-lg mx-auto px-4 py-8">
+      <div className="container max-w-screen-lg mx-auto py-8">
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
           {/* Sidebar */}
           <div className="md:sticky md:top-24 h-fit">
